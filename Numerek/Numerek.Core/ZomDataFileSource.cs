34b08f11316375ee57032eca4578179dc256504d
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:47:53 -0200

    Add ZomDataFileSource.cs

---
+++ Numerek/Numerek.Core/ZomDataFileSource.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Numerek.Core;
+namespace Numerek.Core
+{
+    public partial class ZomDataFileSource : IDataSource
+    {
+        /// <summary>
+        /// Take data from File
+        /// </summary>
+        public Kolejki GetData()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Kolejki));
+
+            using (FileStream fileStream = File.Open(@"C:\WORK SPACE\Code Me workspace\numerek_repository\Numerek\Numerek.Tests\qmatic-zom4.xml", FileMode.Open))
+            {
+                var data = (Kolejki)serializer.Deserialize(fileStream);
+                return data; //!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+
+
+        }
+        /// <summary>
+        /// Takes data from file
+        /// </summary>
+        /// <param name="zomNumber">Numer zespolu obłsugi klienta</param>
+        /// <returns>Kolejki type</returns>
+        public Kolejki GetData(int zomNumber)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Kolejki));
+
+            using (FileStream fileStream = File.Open($@"C:\WORK SPACE\Code Me workspace\numerek_repository\Numerek\Numerek.Tests\qmatic-zom{zomNumber}.xml", FileMode.Open))
+            {
+                var data = (Kolejki)serializer.Deserialize(fileStream);
+                return data; //!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
+
+
+        }
+
+
+    }
+}
