717fba91f5db9ecc2adfa740e3cee43afea8f098
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:49:22 -0200

    Add ZomJsonOutputStrategy

---
+++ Numerek/Numerek.Core/ZomJsonOutputStrategy.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+namespace Numerek.Core
+{
+    /// <summary>
+    /// Serialize results to JSON
+    /// </summary>
+    public class ZomJsonOutputStrategy
+    {
+        /// <summary>
+        /// Method take Kolejki type as parameter and serialize it to JSON string
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>string type with JSON</returns>
+        public string Execute(Kolejki data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            Console.WriteLine(result);
+            return result;
+
+        }
+        
+    }
+}
