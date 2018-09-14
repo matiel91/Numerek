7fe4a302eb29eb26be1eb72554268a2433380c44
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:48:58 -0200

    Add ZommHumanReadableOutputStrategy.cs

---
+++ Numerek/Numerek.Core/ZomHumanReadableOutputStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerek.Core
+{
+    /// <summary>
+    /// Human readable results
+    /// </summary>
+    public class ZomHumanReadableOutputStrategy
+    {
+        /*
+        public string Execute(Kolejki data)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in data.Okienka)
+            {
+                //Correction Required - Need to equalize column => .PadRight() and correct unit tests
+                result.AppendLine($"{item.NazwaGrupy.Trim()}: Liczba osób w kolejce: {item.LiczbaOsobWKolejce}, Czas Oczekiwania: {item.CzasOczekiwania}min");
+            }
+
+            return result.ToString();
+        }
+        */
+        public string Execute(Kolejki data)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (var item in data.Okienka)
+            {
+                //Correction Required - Need to equalize column => .PadRight() and correct unit tests
+                result.AppendLine($"{item.NazwaGrupy.Trim()}: Liczba osób w kolejce: {item.LiczbaOsobWKolejce}, Czas Oczekiwania: {item.CzasOczekiwania}min");
+            }
+
+            return result.ToString();
+        }
+    }
+   
+}
