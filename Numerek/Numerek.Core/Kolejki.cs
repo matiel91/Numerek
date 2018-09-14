b7c776d93c232194f2f8f3aff1e612c401d12c31
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:36:00 -0200

    Add generated Kolejki.cs and Okienko.cs

---
+++ Numerek/Numerek.Core/Kolejki.cs
@@ -0,0 +1,21 @@
+﻿/* 
+   Licensed under the Apache License, Version 2.0
+
+   http://www.apache.org/licenses/LICENSE-2.0
+   */
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace Numerek.Core
+{
+    [XmlRoot(ElementName = "ROOT")]
+    public class Kolejki
+    {
+        [XmlElement(ElementName = "GRUPY")]
+        public List<Okienko> Okienka { get; set; }
+        [XmlAttribute(AttributeName = "time")]
+        public string Time { get; set; }
+        [XmlAttribute(AttributeName = "date")]
+        public string Date { get; set; }
+    }
+
+}
---
+++ Numerek/Numerek.Core/Okienko.cs
@@ -0,0 +1,33 @@
+﻿/* 
+   Licensed under the Apache License, Version 2.0
+
+   http://www.apache.org/licenses/LICENSE-2.0
+   */
+using System;
+using System.Xml.Serialization;
+namespace Numerek.Core
+{
+    [XmlRoot(ElementName = "GRUPY")]
+    public class Okienko 
+    {
+        [XmlElement(ElementName = "LP")]
+        public string LP { get; set; }
+        [XmlElement(ElementName = "IDGRUPY")]
+        public string IdGrupy { get; set; }
+        [XmlElement(ElementName = "LITERAGRUPY")]
+        public string LiteraGrupy { get; set; }
+        [XmlElement(ElementName = "NAZWAGRUPY")]
+        public string NazwaGrupy { get; set; }
+        [XmlElement(ElementName = "AKTUALNYNUMER")]
+        public string AktualnyNumer { get; set; }
+        [XmlElement(ElementName = "LICZBAKLWKOLEJCE")]
+        public string LiczbaOsobWKolejce { get; set; }
+        [XmlElement(ElementName = "LICZBACZYNNYCHSTAN")]
+        public string IloscCzynnychStanowisk { get; set; }
+        [XmlElement(ElementName = "CZASOBSLUGI")]
+        public string CzasOczekiwania { get; set; }
+        [XmlAttribute(AttributeName = "status")]
+        public string Status { get; set; }
+    }
+
+}
