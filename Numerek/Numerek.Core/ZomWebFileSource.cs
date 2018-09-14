48622df7e6d2576de2c7da183b3b9937a3637000
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:48:11 -0200

    Add ZomWebFileSource.cs

---
+++ Numerek/Numerek.Core/ZomWebFileSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Xml.Serialization;
+namespace Numerek.Core
+{
+    /// <summary>
+    /// Downloading data from website
+    /// </summary>
+    public class ZomWebFileSource : IDataSource
+    {
+        /// <summary>
+        /// Geting data from website
+        /// </summary>
+        /// <param name="zomNumber">Numer zespołu obsługi klienta</param>
+        /// <returns>Kolejki</returns>
+        public Kolejki GetData(int zomNumber)
+        {
+            if (zomNumber < 0 || zomNumber > 4)
+            {
+                throw new Exception("Zly numer ZOM");
+            }
+            var client = new HttpClient();
+            XmlSerializer serializer = new XmlSerializer(typeof(Kolejki));
+            var xmlStream = client.GetStreamAsync($"http://www.gdansk.pl/files/xml/qmatic-zom{zomNumber}.xml").Result;
+            Kolejki result = (Kolejki)serializer.Deserialize(xmlStream);
+
+            return result;
+        }
+    }
+}
