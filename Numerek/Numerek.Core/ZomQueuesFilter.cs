d2c1db5838f25e0bd1092b3e28dd94e575dd79b2
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:49:41 -0200

    Add ZomQueuesFilter.cs

---
+++ Numerek/Numerek.Core/ZomQueuesFilter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Numerek.Core;
+
+namespace Numerek.Core
+{
+    /// <summary>
+    /// Data filters
+    /// </summary>
+    public class ZomQueuesFilter
+    {
+        /// <summary>
+        /// Data filter
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>Filtered data where queue > 1</returns>
+        public Kolejki Filter(Kolejki data)
+        {
+            //How to do with LINQ???
+            //var moreThenOne = data.Grupa.Where<Grupa>(x => int.Parse(x.LiczbaOsobWKolejce) > 1);
+            Kolejki kolejki = new Kolejki
+            {
+               Okienka = new List<Okienko>() 
+            };
+            foreach (var item in data.Okienka)
+            {
+                if (int.Parse(item.LiczbaOsobWKolejce) > 1)
+                {
+                    kolejki.Okienka.Add(item);
+                }
+            }
+            return kolejki;
+                
+        }
+    }
+}
---
+++ Numerek/Numerek/Program.cs
@@ -0,0 +1,27 @@
+﻿using Numerek.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var zomDataFileSource = new ZomDataFileSource().GetData(3);
+            var output = new ZomHumanReadableOutputStrategy();
+            Console.WriteLine(output.Execute(zomDataFileSource));
+            */
+
+            var numerek = new NumerekFlow(new ZomWebFileSource());
+            var output = new ZomHumanReadableOutputStrategy();
+            Console.WriteLine("Podaj numer ZOM 1-4");
+
+            Console.WriteLine(output.Execute(numerek.Execute(int.Parse(Console.ReadLine()))));
+            Console.ReadLine();
+        }
+    }
+}
