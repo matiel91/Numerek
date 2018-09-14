ed937aa8003d3ce216746301c6dce1a3bf0274d2
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-09-13 23:48:35 -0200

    Add NumerekFlow.cs

---
+++ Numerek/Numerek.Core/NumerekFlow.cs
@@ -0,0 +1,21 @@
+﻿namespace Numerek.Core
+{
+    /// <summary>
+    /// Constructor implement IDataSource type. It can be ZomDataFileSource or ZomWebFileSource which implements IDataSource interface.
+    /// Strategy desing pattern
+    /// </summary>
+    public class NumerekFlow
+    {
+        private readonly IDataSource dataSource;
+
+        public NumerekFlow(IDataSource dataSource)
+        {
+            this.dataSource = dataSource;
+        }
+
+        public Kolejki Execute(int zomNumber)
+        {
+            return dataSource.GetData(zomNumber);
+        }
+    }
+}
